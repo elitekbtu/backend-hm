e32d1239918a3844e48853dd10bab3b9e50a529a
elitekbtu <satbaldievst@mail.ru>
2025-06-12 17:40:05 +0500

    new commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Enable CORS for frontend (e.g. http://localhost:5173). In production, restrict origins.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://161.35.209.111/", "http://161.35.209.111:8000"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://161.35.209.111", "http://161.35.209.111:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
